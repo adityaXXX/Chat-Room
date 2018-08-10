33f61804c059d1bd3f93c7b431696dd578209102
Aditya Kumar <adityabitw108@gmail.com>
2018-08-10 23:20:01 +0530

    bugs again :(

--- server.py
+++ server.py
@@ -13,29 +13,34 @@ def Connections():
         print("{} is connected!!".format(addr))
         client.send(("Welcome to Chat Room. Type {quit} to exit. Enter your name: ").encode("utf-8"))
         addresses[client] = addr
-        Thread(target = ClientConnection, args=(client)).start()
+        Thread(target = ClientConnection, args=(client, )).start()
 
 def ClientConnection(client):
     name = client.recv(BufferSize).decode("utf-8")
     client.send(("Hello {}".format(name)).encode("utf-8"))
-    Broadcast(("{} has joined the chat..").format(name))
+    message = ("{} has joined the chat..").format(name)
+    Broadcast(message.encode("utf-8"))
     clients[client] = name
     while True:
         msg = client.recv(BufferSize).decode("utf-8")
-        if msg != quit:
-            Broadcast(msg, name + ": ")
+        if msg != "quit":
+            Broadcast(msg.encode("utf-8"), name + ": ")
         else:
-            Broadcast(("{} has left the chat.").format(clients[client]))
+            message = ("{} has left the chat.").format(clients[client])
+            Broadcast(message.encode("utf-8"))
             client.send(("Will see you soon..").encode("utf-8"))
             del clients[client]
             break
 
 def Broadcast(msg, name = ""):
     for sockets in clients:
-        socket.send((name + msg).encode("utf-8"))
+        sockets.send(name.encode("utf-8") + msg)
 
 server = socket(family=AF_INET, type=SOCK_STREAM)
-socket.bind((HOST, PORT))
+try:
+    server.bind((HOST, PORT))
+except OSError:
+    print("Server Busy")
 BufferSize = 1024
 
 server.listen(5)
