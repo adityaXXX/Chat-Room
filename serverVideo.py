27de1cb0985e72d160df0319a1d8571ddb79d903
Aditya Kumar <adityabitw108@gmail.com>
2018-08-16 21:01:31 +0530

    some changes made...

--- serverVideo.py
+++ serverVideo.py
@@ -3,8 +3,8 @@ from threading import Thread
 
 import numpy as np
 
-HOST = "192.168.157.206"
-PORT = 3000
+HOST = "192.168.43.215"
+PORT = 5000
 BufferSize = 4096
 addresses = {}
 
@@ -12,7 +12,7 @@ def Connections():
     while True:
         client, addr = server.accept()
         print("{} is connected!!".format(addr))
-        client.send(("Welcome to Chat Room. Press {q} to exit").encode("utf-8"))
+        # client.send(("Welcome to Chat Room. Press {q} to exit").encode("utf-8"))
         addresses[client] = addr
         Thread(target=ClientConnection, args=(client, )).start()
 
@@ -20,25 +20,29 @@ def ClientConnection(client):
     length = int(client.recv(2 * BufferSize).decode("utf-8"))
     SendLength(client, str(length))
     while True:
+        # print("In client Connections...")
         receivingBuffer = client.recv(BufferSize)
         if not receivingBuffer:
             break
         data = b''
         if len(data) < length:
-            data = client.recv(BufferSize if to_read > BufferSize else to_read)
+
             to_read = length - len(data)
+            data = client.recv(BufferSize if to_read > BufferSize else to_read)
             length -= len(data)
         broadcast(client, data)
 
 def SendLength(clientSocket, length):
     for client in addresses:
         if client != clientSocket:
+            print("sending length:- {}".format(len(length)))
             client.send(length.encode("utf-8"))
 
 
 def broadcast(clientSocket, receivingBuffer):
     for client in addresses:
         if client != clientSocket:
+            # print("Broadcasting...")
             client.send(receivingBuffer)
 
 server = socket(family=AF_INET, type=SOCK_STREAM)
