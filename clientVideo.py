53f7531f6b27aa31c618a8805c5389446ac8496e
Aditya Kumar <adityabitw108@gmail.com>
2018-08-20 23:13:59 +0530

    Minor Bug

--- clientVideo.py
+++ clientVideo.py
@@ -7,21 +7,22 @@ import pyaudio
 from array import array
 
 HOST = "192.168.157.206"
-PORT = 4000
+PORT = 3000
 BufferSize = 4096
 
 FORMAT=pyaudio.paInt16
 CHANNELS=2
 RATE=44100
 CHUNK=1024
-ln = 640*480*3 + 4*CHUNK + 3
+lnF = 640*480*3
+ln = lnF + 4*CHUNK + 3
 
 def SendMedia():
     while True:
         try:
             frame = wvs.read()
             cv2_im = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = cv2.resize(cv2_im, (640, 480))
+            frame = cv2.resize(frame, (640, 480))
             frame = np.array(frame).reshape(1, lnF)
             jpg_as_text = bytearray(frame)
 
@@ -33,14 +34,13 @@ def SendMedia():
             databytes = jpg_as_text + b'xXx' + data
             print("Sending Media...")
             client.sendall(databytes)
-        except:
-            continue
 
 def RecieveMedia():
     while True:
         try:
             databytes = client.recv(ln)
             img, data = databytes.split(b'xXx')
+            print("Recieving Media..")
             img = list(img)
             img = np.array(img, dtype = np.uint8).reshape(480, 640, 3)
             cv_image = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
